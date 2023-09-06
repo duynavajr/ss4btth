95ad408cc2a3de911f754dec8ff04a2483ced150
duynavajr <132381004+duynavajr@users.noreply.github.com>
2023-09-06 08:05:26 +0700

    Add files via upload

---
+++ MD5_SS4_TH-main/src/main/java/ra/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package ra.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import ra.model.Customer;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<
+        Customer, Long> {
+}
